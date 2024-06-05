319a3a74df4707c157d51e82cbc1c2ae4fca55c4
Gandharv Talikoti <gandharvtalikoti19@gmail>
2024-06-05 14:04:47 +0530

    Implement getBook by id api

--- Proj/GO api/main/go.mod
+++ Proj/GO api/main/go.mod
@@ -2,6 +2,8 @@ module main
 
 go 1.22.3
 
+require golang.org/x/tools v0.22.0
+
 require (
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
@@ -24,10 +26,10 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.8.0 // indirect
-	golang.org/x/crypto v0.23.0 // indirect
-	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sys v0.20.0 // indirect
-	golang.org/x/text v0.15.0 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- Proj/GO api/main/main.go
+++ Proj/GO api/main/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	// "errors"
+	"errors"
 	// "fmt"
-	"github.com/gin-gonic/gin"
 	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
 // we'll build a library api
@@ -27,23 +27,45 @@ var books = []book{
 	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
 	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
 }
-func getBOoks(c *gin.Context){
-	c.IndentedJSON(http.StatusOK, books);
+
+func getBOoks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, books)
 }
 
-func createBook(c *gin.Context){
-	 var newBook book
-	 if err:= c.BindJSON(&newBook); err != nil{
+func createBook(c *gin.Context) {
+	var newBook book
+	if err := c.BindJSON(&newBook); err != nil {
 		return
-	 }
+	}
 
-	 books = append(books, newBook)
-	 c.IndentedJSON(http.StatusCreated, newBook)
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
+// getting a book by id
+func getBookById(id string) (*book, error) {
+	for i, b := range books {
+		if b.ID == id {
+			return &books[i], nil
+		}
+	}
+	return nil, errors.New("book not found")
+}
+
+func bookById(c *gin.Context) {
+	id := c.Param("id") // "/books/2"
+	book, err := getBookById(id)
+	if err != nil {
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, book)
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/books", getBOoks)
 	router.POST("/books", createBook)
+	router.GET("/books/:id", bookById)
 	router.Run("localhost:8080")
 }
