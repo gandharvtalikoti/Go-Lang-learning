055d3497d259f248b6066f5c2e0acbc356d81749
Gandharv Talikoti <gandharvtalikoti19@gmail>
2024-06-05 11:00:01 +0530

    structure of book, dummy data

--- Proj/GO api/main/main.go
+++ Proj/GO api/main/main.go
@@ -1,6 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// we'll build a library api
+// - store a bunch of books
+// - check in a book
+// - check out a book
+// - view all books
+// - get books by id
+
+type book struct {
+	ID       string `json:"id"` // json field initializer
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Quantity int    `json:"quantity"`
+}
+
+// dummy data
+var books = []book{
+	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
+	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
+	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
+}
 
 func main() {
 	fmt.Println("API")
